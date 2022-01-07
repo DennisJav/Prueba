f2f09bfc5e38d1cfd5b721615b5dcb733bac7df3
Dennis <Dennis@DESKTOP-D84QG4M>
2022-01-07 16:54:02 -0500

    hola

---
+++ src/main/java/ec/edu/Prueba1Application.java
@@ -0,0 +1,14 @@
+package ec.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prueba1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prueba1Application.class, args);
+		System.out.println("hola");
+	}
+
+}
